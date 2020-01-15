3d64238205de5f9f37136ed783e4ea47469fc611
Sreekanth Reddy <sreekanthreddy.iragamreddy@united.com>
2020-01-15 13:38:18 -0600

    Initial Commit

---
+++ SampleWatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWatchApp
+//
+//  Created by sreekanth reddy iragam reddy on 1/15/20.
+//  Copyright © 2020 SampleWatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
